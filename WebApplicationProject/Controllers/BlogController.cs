fc8cf8022a13c566d4abcd5988a3ca81f6437e51
argelamilo <argelam29@yahoo.com>
2024-03-08 03:59:48 +0100

    changes (08/03/2024)

---
+++ WebApplicationProject/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplicationProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult BlogEntries()
+        {
+            return View();
+        }
+
+        public ActionResult PostDetails()
+        {
+            return View();
+        }
+    }
+}
